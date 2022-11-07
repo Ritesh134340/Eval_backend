6eb87939807786339b6e71485fe00fda01a4b5f8
Ritesh134340 <riteshkumar134340@gmail.com>
2022-11-07 16:54:14 +0530

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express=require("express");
 const connection=require("./config/db");
 const app=express()
+var cors = require('cors')
 require("dotenv").config();
 const PORT=process.env.PORT || 8080
 app.use(express.json());
 const userRouter=require("./routes/user.route")
 const todoRouter=require("./routes/todo.router")
-
+app.use(cors())
 app.use("/user",userRouter);
 app.use("/todo",todoRouter)
 
